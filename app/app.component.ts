e36169812b6a48cebc88eb43861f787563f2f32d
Dominic Brown and Mohamed Warsame <mohamed@>
2017-06-08 16:48:43 -0700

    add keg fullness

--- app/app.component.ts
+++ app/app.component.ts
@@ -6,19 +6,18 @@ import { Keg } from './keg.model';
   selector: 'app-root',
   template: `
   <div class="container">
-
-      <keg-list [childKegList]="masterKegList" (clickSender1)="editKeg($event)" (clickSender2)="showKeg($event)"></keg-list>
-
     <div class="row">
-      <div class="col-sm-6">
-        <keg-detail [childSelectedKegDetail]="selectedKegDetail" (hideButtonClickedSender)="hideKeg()" (sellPintClickedSender)="sellOnePint($event)" (refillKegClickedSender)="refillKeg($event)"></keg-detail>
-      </div>
+      <keg-list [childKegList]="masterKegList" (clickSender1)="editKeg($event)" (clickSender2)="showKeg($event)"></keg-list>
     </div>
-
     <hr>
-    <edit-keg [childSelectedKeg]="selectedKeg" (doneButtonClickedSender)="finishedEditing()" ></edit-keg>
-    <new-keg (newKegSender)="addKeg($event)"></new-keg>
-
+    <div class="row">
+      <keg-detail id="details" [childSelectedKegDetail]="selectedKegDetail" (hideButtonClickedSender)="hideKeg()" (sellPintClickedSender)="sellOnePint($event)" (refillKegClickedSender)="refillKeg($event)"></keg-detail>
+    </div>
+    <hr>
+    <div class="row">
+      <edit-keg [childSelectedKeg]="selectedKeg" (doneButtonClickedSender)="finishedEditing()" ></edit-keg>
+      <new-keg (newKegSender)="addKeg($event)"></new-keg>
+    </div>
   </div>
   `
 })
@@ -26,14 +25,15 @@ import { Keg } from './keg.model';
 
 export class AppComponent {
   masterKegList: Keg[] = [ new Keg('Mirror Pond Pale Ale', 'Deschutes Brewery', 98, 4.5, "http://thefullpint.com/wp-content/uploads/2008/07/deschutes.jpg"),
-                  new Keg('Blue Dot IPA', 'Hair of the Dog', 112, 7, "http://www.bringonthebeer.com/images/hair_of_the_dog_brewing_blue_dot_full.jpg"),
-                  new Keg('Budweiser Light', 'Budweiser', 125, 4, "http://www.boozingear.com/media/catalog/product/cache/7/image/650x/040ec09b1e35df139433887a97daa66f/b/u/bud-0178b.jpg"),
-                  new Keg('Pabst Blue Ribbon', 'Pabst', 110, 3.5, "https://upload.wikimedia.org/wikipedia/en/thumb/c/cd/Pabst_Blue_Ribbon_logo.svg/911px-Pabst_Blue_Ribbon_logo.svg.png"),
-                  new Keg('Apocalypse IPA', '10 Barrel', 189, 6.8, "https://cdn.beeradvocate.com/im/beers/53808.jpg"),
-                ];
+                           new Keg('Blue Dot IPA', 'Hair of the Dog', 112, 7, "http://www.bringonthebeer.com/images/hair_of_the_dog_brewing_blue_dot_full.jpg"),
+                           new Keg('Budweiser Light', 'Budweiser', 125, 4, "http://www.boozingear.com/media/catalog/product/cache/7/image/650x/040ec09b1e35df139433887a97daa66f/b/u/bud-0178b.jpg"),
+                           new Keg('Pabst Blue Ribbon', 'Pabst', 110, 3.5, "https://upload.wikimedia.org/wikipedia/en/thumb/c/cd/Pabst_Blue_Ribbon_logo.svg/911px-Pabst_Blue_Ribbon_logo.svg.png"),
+                           new Keg('Apocalypse IPA', '10 Barrel', 189, 6.8, "https://cdn.beeradvocate.com/im/beers/53808.jpg"),
+                          ];
   selectedKeg: null;
   // used to be "selectedKegDetail: null;" but this caused build errors
   selectedKegDetail: Keg;
+  currentPintsLeft: number = 120;
 
   showKeg(clickedKegDetail) {
     this.selectedKegDetail = clickedKegDetail;
@@ -67,3 +67,7 @@ export class AppComponent {
   refillKeg() {
     this.selectedKegDetail.pintsLeft = 124;
   }
+
+
+
+}
--- app/keg-detail.component.ts
+++ app/keg-detail.component.ts
@@ -5,18 +5,29 @@ import { Keg } from './keg.model';
 @Component({
   selector: 'keg-detail',
   template: `
-    <div *ngIf="childSelectedKegDetail">
-      <ul>
-        <li> <b>Name:</b>  {{childSelectedKegDetail.name}} </li>
-        <li> <b>Brand:</b>   {{childSelectedKegDetail.brand}} </li>
-        <li> <b>Keg Price:</b>   \${{childSelectedKegDetail.price}}\.00 </li>
-        <li> <span [class]="abvColor(childSelectedKegDetail)"><b>ABV:</b></span>   {{childSelectedKegDetail.abv}}\% </li>
-        <li> <b>Pints left:</b>   {{childSelectedKegDetail.pintsLeft}} </li>
-        <li> <b>Price per pint:</b>   \${{childSelectedKegDetail.pintPrice}} </li>
-      </ul>
-      <button class="btn btn-default btn-sm" (click)="hideButtonClicked()">Hide Details</button>
-      <button class="btn btn-success btn-sm" (click)="sellPintClicked()">Sell Pint</button>
-      <button class="btn btn-primary btn-sm" (click)="refillKegClicked()">Refill Keg</button>
+    <div>
+      <div  *ngIf="childSelectedKegDetail">
+        <div class="col-sm-5">
+          <ul>
+            <li> <b>Name:</b>  {{childSelectedKegDetail.name}} </li>
+            <li> <b>Brand:</b>   {{childSelectedKegDetail.brand}} </li>
+            <li> <b>Keg Price:</b>   \${{childSelectedKegDetail.price}}\.00 </li>
+            <li> <span [class]="abvColor(childSelectedKegDetail)"><b>ABV:</b></span>   {{childSelectedKegDetail.abv}}\% </li>
+            <li> <b>Pints left:</b>   {{childSelectedKegDetail.pintsLeft}} </li>
+            <li> <b>Price per pint:</b>   \${{childSelectedKegDetail.pintPrice}} </li>
+          </ul>
+          <button class="btn btn-default btn-sm" (click)="hideButtonClicked()">Hide Details</button>
+          <button class="btn btn-success btn-sm" (click)="sellPintClicked()">Sell Pint</button>
+          <button class="btn btn-primary btn-sm" (click)="refillKegClicked()">Refill Keg</button>
+        </div>
+        <div class="col-sm-5">
+          <div  id="kegFullnessDiv">
+            <img id="kegFullness" src="public/img/barrel2.png" alt="Keg fullness">
+            <div id="kegFill" [style.margin-top]="changeFullness(childSelectedKegDetail.pintsLeft)"            [style.height]="changeFullnessHeight(childSelectedKegDetail.pintsLeft)">
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   `
 })
@@ -48,6 +59,32 @@ export class KegDetailComponent {
     } else {
       return  "abvHigh";
     }
+  }
+
+  // changeFullness(childSelectedKegDetail)
+  changeFullness(myPintsLeft) {
+    var barrelFullness: number = 0;
+    if (myPintsLeft >= 100) {
+      barrelFullness = 30;
+    } else if ((myPintsLeft < 100) && (myPintsLeft >= 50)) {
+      barrelFullness = 80;
+    } else {
+      barrelFullness = 115;
+    }
+    return barrelFullness;
+  }
+
+  changeFullnessHeight(myPintsLeft) {
+    var barrelFullnessHeight: number = 0;
+    // barrelFullnessHeight = ((.24 * 50) + 30)
+    if (myPintsLeft >= 100) {
+      barrelFullnessHeight = 110;
+    } else if ((myPintsLeft < 100) && (myPintsLeft >= 50)) {
+      barrelFullnessHeight = 60;
+    } else {
+      barrelFullnessHeight = 30;
+    }
+    return barrelFullnessHeight;
   }
 
 }
